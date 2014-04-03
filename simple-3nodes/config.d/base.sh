a30506fec355828a402d955328e40520a9eb63c2
Masahito Yoshida <masahito@axsh.net>
2014-04-03 14:56:51 +0900

    configure node-specific ip address for eth1.

--- simple-3nodes/config.d/base.sh
+++ simple-3nodes/config.d/base.sh
@@ -8,3 +8,19 @@ set -o pipefail
 set -x
 
 # Do some changes ...
+
+ifcfg_setup=/usr/local/bin/ifcfg-setup
+
+if [[ ! -f ${ifcfg_setup} ]]; then
+  curl -fSkL https://raw.githubusercontent.com/hansode/ifcfg-setup/master/bin/ifcfg-setup -o ${ifcfg_setup}
+  chmod +x ${ifcfg_setup}
+fi
+
+case "$(hostname)" in
+node01) ip=192.168.1.11 ;;
+node02) ip=192.168.1.12 ;;
+node03) ip=192.168.1.13 ;;
+esac
+
+${ifcfg_setup} install ethernet eth1 ip=${ip}
+ifup eth1
