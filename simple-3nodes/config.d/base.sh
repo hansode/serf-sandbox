2c08936bad576ef9bd5cab35126e3ad5f8a8eb37
Masahito Yoshida <masahito@axsh.net>
2014-04-10 13:05:19 +0900

    update ifcfg-setup download url.

--- simple-3nodes/config.d/base.sh
+++ simple-3nodes/config.d/base.sh
@@ -12,7 +12,7 @@ set -x
 ifcfg_setup=/usr/local/bin/ifcfg-setup
 
 if [[ ! -f ${ifcfg_setup} ]]; then
-  curl -fSkL https://raw.githubusercontent.com/hansode/ifcfg-setup/master/bin/ifcfg-setup -o ${ifcfg_setup}
+  curl -fSkL https://raw.githubusercontent.com/hansode/ifutils/master/bin/ifcfg-setup -o ${ifcfg_setup}
   chmod +x ${ifcfg_setup}
 fi
 
